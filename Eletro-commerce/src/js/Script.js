a49430a36c33aafc21d57413ff416b41fd27871b
paulo welton <paulinho.welton08@gmail.com>
2024-09-19 00:20:29 -0300

    adicao da biblioteca sweet alert

--- Eletro-commerce/src/js/Script.js
+++ Eletro-commerce/src/js/Script.js
@@ -37,3 +37,15 @@ function update(){
     sliderTrocaImagem()
 }
 let intervalo = setInterval(update, 4000)
+
+let btnLogin = document.querySelector(".login")
+
+btnLogin.addEventListener("click", ()=>{
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        timer: 1500
+      });
+})
